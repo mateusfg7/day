14d21aa2a2d609c9ae4e31fd73a7d8ac2d20744e
mateusfg7 <mateusfg77@gmail.com>
2019-08-25 17:35:22 -0300

    add: wallpaper switch

--- main.js
+++ main.js
@@ -2,25 +2,38 @@ const data = new Date;
 //const hora = data.getHours();
 const hora = 5;
 
+const body = $('body');
 const image = $('#image');
 
 
 if (hora >= 6 && hora < 11) {
-    image.attr('src', 'img/morning_icon.jpg');
+    body.addClass('morningBody');
+
+    image.attr('src', 'img/icon/morning_icon.jpg');
     image.attr('alt', 'Morning Icon');
 } else if ( hora >= 11 && hora < 13 ) {
-    image.attr('src', 'img/lunch_icon.jpg');
+    body.addClass('lunchBody');
+
+    image.attr('src', 'img/icon/lunch_icon.jpg');
     image.attr('alt', 'Lunch Icon');
 } else if ( hora >= 13 && hora <= 16 ) {
-    image.attr('src', 'img/evening_icon.jpg');
+    body.addClass('eveningBody');
+
+    image.attr('src', 'img/icon/evening_icon.jpg');
     image.attr('alt', 'Evening Icon');
 } else if ( hora > 16 && hora <= 18 ) {
-    image.attr('src', 'img/sunset_icon.jpg');
+    body.addClass('sunsetBody');
+
+    image.attr('src', 'img/icon/sunset_icon.jpg');
     image.attr('alt', 'Sunset Icon');
 } else if ( hora > 18 || hora <= 4 ) {
-    image.attr('src', 'img/night_icon.jpg');
+    body.addClass('nightBody');
+
+    image.attr('src', 'img/icon/night_icon.jpg');
     image.attr('alt', 'Night Icon');
 } else if ( hora > 4 && hora < 6 ) {
-    image.attr('src', 'img/dawn_icon.jpg');
+    body.addClass('dawnBody');
+
+    image.attr('src', 'img/icon/dawn_icon.jpg');
     image.attr('alt', 'Daw Icon');
 }
