e431dcaddb9608e7fe801a0cc93310d9289af028
mateusfg7 <mateusfg77@gmail.com>
2019-08-26 17:12:36 -0300

    refactored

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const data = new Date;
-//const hora = data.getHours();
-const hora = 5;
+const hora = data.getHours();
 
 const condition = hora === 1 ? `é ${hora} hora`:`são ${hora} horas`;
 
@@ -13,6 +12,7 @@ const image = $('#image');
 
 
 if (hora >= 6 && hora < 11) {
+
     title.html('Morning');
     saudacao.html('Bom Dia!');
     hour.html(`Agora são ${hora} horas`);
@@ -20,9 +20,11 @@ if (hora >= 6 && hora < 11) {
     body.addClass('morningBody');
     area.addClass('morningArea');
 
-    image.attr('src', 'img/icon/morning_icon.jpg');
+    image.attr('src', 'img/morning_icon.jpg');
     image.attr('alt', 'Morning Icon');
+
 } else if ( hora >= 11 && hora < 13 ) {
+
     title.html('Lunch');
     saudacao.html('Bom Dia!');
     hour.html(`Agora são ${hora} horas`);
@@ -30,9 +32,11 @@ if (hora >= 6 && hora < 11) {
     body.addClass('lunchBody');
     area.addClass('lunchArea');
 
-    image.attr('src', 'img/icon/lunch_icon.jpg');
+    image.attr('src', 'img/lunch_icon.jpg');
     image.attr('alt', 'Lunch Icon');
+
 } else if ( hora >= 13 && hora <= 16 ) {
+
     title.html('Evening');
     saudacao.html('Boa Tarde!');
     hour.html(`Agora são ${hora} horas`);
@@ -40,9 +44,11 @@ if (hora >= 6 && hora < 11) {
     body.addClass('eveningBody');
     area.addClass('eveningArea');
 
-    image.attr('src', 'img/icon/evening_icon.jpg');
+    image.attr('src', 'img/evening_icon.jpg');
     image.attr('alt', 'Evening Icon');
+
 } else if ( hora > 16 && hora <= 18 ) {
+
     title.html('Sunset');
     saudacao.html('Boa Tarde!');
     hour.html(`Agora são ${hora} horas`);
@@ -50,9 +56,11 @@ if (hora >= 6 && hora < 11) {
     body.addClass('sunsetBody');
     area.addClass('sunsetArea');
 
-    image.attr('src', 'img/icon/sunset_icon.jpg');
+    image.attr('src', 'img/sunset_icon.jpg');
     image.attr('alt', 'Sunset Icon');
+
 } else if ( hora > 18 || hora <= 4 ) {
+
     title.html('Night');
     saudacao.html('Boa Noite!');
     hour.html(`Agora ${condition}`);
@@ -60,9 +68,11 @@ if (hora >= 6 && hora < 11) {
     body.addClass('nightBody');
     area.addClass('nightArea');
 
-    image.attr('src', 'img/icon/night_icon.jpg');
+    image.attr('src', 'img/night_icon.jpg');
     image.attr('alt', 'Night Icon');
+
 } else if ( hora > 4 && hora < 6 ) {
+
     title.html('Daw');
     saudacao.html('Boa Noite!');
     hour.html(`Agora são ${hora} horas`);
@@ -70,6 +80,7 @@ if (hora >= 6 && hora < 11) {
     body.addClass('dawnBody');
     area.addClass('dawnArea');
 
-    image.attr('src', 'img/icon/dawn_icon.jpg');
+    image.attr('src', 'img/dawn_icon.jpg');
     image.attr('alt', 'Daw Icon');
+
 }
